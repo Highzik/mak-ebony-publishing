cce8f2bbcfa4d125acda6d5223c4e92c47b03108
Highzik <ajadiisaac99@gmail.com>
2023-10-12 21:43:26 +0100

    I added a carousel effect to give the project more appealing designs to the eyes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from './components/About';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import SlideInPage from './components/SlideInPage';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Features />
       <About />
       <Testimonials />
+      <SlideInPage />
       <Contact />
       <Footer />
     </>
---
+++ src/components/SlideInPage.jsx
@@ -0,0 +1,30 @@
+import '/src/styles/slideinpage.css'
+const SlideInPage = () => {
+  return (
+    <div id="carouselExampleAutoplaying" className="carousel slide container-fluid" data-bs-ride="carousel">
+      <div className="carousel-inner container justify-content-center">
+        <div className="carousel-item active">
+          <img src="src/images/img-3.jpg" className="img-fluid logo-7" alt="" />
+        </div>
+        <div className="carousel-item">
+          <img src="src/images/img-2.jpg" className="img-fluid logo-8" alt="" />
+        </div>
+        <div className="carousel-item">
+          <img src="src/images/img-4.jpg" className="img-fluid logo-9" alt="" />
+        </div>
+      </div>
+      <div className="container justify-content-center">
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default SlideInPage
